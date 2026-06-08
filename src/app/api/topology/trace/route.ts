83466fd058de0360ad6fc10710a75ebb9698e0c6
Abiel Kim <abielkim@hotmail.com>
2026-06-07 22:38:52 -0700

    feat: implemented localTracert function in /api/topology/trace for Windows. Trace api now fully functional.

--- src/app/api/topology/trace/route.ts
+++ src/app/api/topology/trace/route.ts
@@ -26,7 +26,7 @@ async function localTraceroute(target: string, send: (hop: Hop) => void) {
   try {
     await new Promise<void>((resolve, reject) => {
       const proc = spawn("traceroute", [target], {
-        // ignore stdio, pipe stdout and stderr
+        // ignore stdin, pipe stdout and stderr
         stdio: ["ignore", "pipe", "pipe"],
       });
 
@@ -42,7 +42,7 @@ async function localTraceroute(target: string, send: (hop: Hop) => void) {
       //  5  dns.google (8.8.8.8)  22.123 ms  21.456 ms  23.789 ms
       rl.on("line", (line: string) => {
         // TODO: send to scan log
-        console.log(`${line}`);
+        console.log(`line: ${line}`);
 
         let ttl: number = 0;
         let ip: string | null = null;
@@ -116,7 +116,108 @@ async function localTraceroute(target: string, send: (hop: Hop) => void) {
 }
 
 // Windows: tracert <target>
-async function localTracert(target: string, send: (hop: Hop) => void) {}
+async function localTracert(target: string, send: (hop: Hop) => void) {
+  try {
+    await new Promise<void>((resolve, reject) => {
+      const proc = spawn("tracert", [target], {
+        // ignore stdin, pipe stdout and stderr
+        stdio: ["ignore", "pipe", "pipe"],
+      });
+
+      const rl: readline.Interface = readline.createInterface({
+        input: proc.stdout,
+      });
+
+      // Tracing route to dns.google [8.8.8.8]
+      // over a maximum of 30 hops:
+
+      //   1     1 ms     2 ms     1 ms  gateway.local [192.168.1.1]
+      //   2    11 ms    12 ms    10 ms  10.10.0.1
+      //   3     *        *        *     Request timed out.
+      //   4    18 ms    20 ms    19 ms  72.14.209.1
+      //   5    22 ms    21 ms    23 ms  dns.google [8.8.8.8]
+
+      // Trace complete.
+
+      rl.on("line", (line: string) => {
+        // TODO: send to scan log
+        console.log(`line: ${line}`);
+
+        let ttl: number = 0;
+        let ip: string | null = null;
+        let hostname: string | null = null;
+        let asn: string | null = null;
+        let rtts: (number | null)[] = [null];
+
+        // NOTE: its possible to define a single regex by making the last bracket-wrapped ip as an optional capture group
+        // but I prefer to avoid dealing with undefined values- define 3 explicit regex instead for the cases of a given hostname,
+        // no hostname, or all rtts are null
+        const regAddr =
+          /^\s*(\d+)\s+((?:\d+\s*ms)|(?:\*))\s+((?:\d+\s*ms)|(?:\*))\s+((?:\d+\s*ms)|(?:\*))\s+(\S+)\s+\[(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\]\s*$/;
+        const regNoAddr =
+          /^\s*(\d+)\s+((?:\d+\s*ms)|(?:\*))\s+((?:\d+\s*ms)|(?:\*))\s+((?:\d+\s*ms)|(?:\*))\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s*$/;
+        const regTimedOut = /^\s*(\d+)\s+(\*\s+\*\s+\*)\s+Request timed out\.$/;
+
+        let match: RegExpMatchArray | null = null;
+
+        if ((match = line.match(regAddr))) {
+          ttl = Number(match[1]);
+          ip = match[6];
+          hostname = match[5];
+          rtts = [match[2], match[3], match[4]].map((rtt) => {
+            if (rtt === "*") return null;
+            return Number(rtt.slice(0, rtt.length - 2).trim());
+          });
+        } else if ((match = line.match(regNoAddr))) {
+          ttl = Number(match[1]);
+          ip = match[5];
+          rtts = [match[2], match[3], match[4]].map((rtt) => {
+            if (rtt === "*") return null;
+            return Number(rtt.slice(0, rtt.length - 2).trim());
+          });
+        } else if ((match = line.match(regTimedOut))) {
+          ttl = Number(match[1]);
+          rtts = [null, null, null];
+        } else {
+          // ignore malformed or irrelevant line
+          return;
+        }
+
+        send({
+          ttl,
+          ip,
+          hostname,
+          asn,
+          rtts,
+        });
+      });
+
+      proc.stderr.on("data", (chunk: Buffer) => {
+        const msg = chunk.toString().trim();
+        // TODO: send to scan log
+        if (msg) console.log(msg);
+      });
+
+      proc.on("error", (err: NodeJS.ErrnoException) => {
+        reject(
+          err.code === "ENOENT"
+            ? new Error(
+                `tracert ${target} failed to start: if tracert is installed please ensure it is in PATH`,
+              )
+            : err,
+        );
+      });
+
+      proc.on("close", () => {
+        // TODO: send to scan log
+        console.log(`tracert ${target} completed`);
+        resolve();
+      });
+    });
+  } catch (err) {
+    console.log(String(err));
+  }
+}
 
 export async function GET(request: NextRequest) {
   const hasTraceroute = !isRemoteDeployment
@@ -154,8 +255,7 @@ export async function GET(request: NextRequest) {
       if (hasTraceroute) {
         await localTraceroute(target, send);
       } else if (hasTracert) {
-        // await localTracert(target, send);
-        await mockTrace(send);
+        await localTracert(target, send);
       } else {
         await mockTrace(send);
       }
